d47e2497924090338d792cbde2c077b5f91808be
Jordan Burger <jordanburger22@gmail.com>
2025-02-03 23:58:56 +0000

    added cors

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const makeRouter = require('./routes/makeRouter')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 
 
 app.use('/wattsbags', makeRouter)
